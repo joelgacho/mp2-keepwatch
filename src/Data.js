d0350fa8d66ffd51877b79a91fc33439e541bd01
joelgacho <joelgacho78@gmail.com>
2023-02-10 20:09:27 +0800

    initialized

---
+++ src/Data.js
@@ -0,0 +1,60 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Rolex Submariner",
+      price: 17982.90,
+      desc: "Features a polished brushed Oystersteel stainless steel case and Oyster bracelet rotating bezel.",
+      img: "images/rolex-1.png",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Rolex Cosmograph Daytona",
+      price: 44435.55,
+      desc: "This 40mm two-tone watch features an Oystersteel case. meteorite",
+      img: "images/rolex-2.png",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "Rolex Daytona Black Diamond dial Oysterflex",
+      price: 65413.72,
+      desc: "This 40mm watch features a solid 18k white gold bezel.",
+      img: "images/rolex-3.png",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Rolex Everose Diamond Gold with Diamonds",
+      price: 19500.30,
+      desc: " combines 904L-grade Oystersteel with 18 karat rose gold.",
+      img: "images/rolex-4.png",
+    },
+    {
+      id: "thiecbawdjksadjk",
+      name: "Rolex Champagne Dial",
+      price: 17400.51,
+      desc: "This luxury watch for ladies features a 31mm stainless steel case.",
+      img: "images/rolex-5.png",
+    },
+    {
+      id: "iuertrywebncdjksadjk",
+      name: "Rolex Submariner Black Dial",
+      price: 15000.42,
+      desc: "polished with brushed 904L Oystersteel stainless steel case.",
+      img: "images/rolex-6.png",
+    },
+    {
+      id: "thierytbvcbvzdhadjk",
+      name: "Rolex 18k Yellow Gold Meteorite",
+      price: 31200.99,
+      desc: "luted bezel and is fitted with a black leather strap with a pattern.",
+      img: "images/rolex-7.png",
+    },
+    {
+      id: "trfoiwfcnbcawdjksadjk",
+      name: "Rolex Milgauss ",
+      price: 12350.75,
+      desc: " Features a polished 904L stainless steel case with a screw-down crown & case stainless oyster bracelet.",
+      img: "images/rolex-8.png"
+    },
+   
+  ];
+  
